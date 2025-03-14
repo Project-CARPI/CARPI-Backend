5a900d8853e3785efa8ebb732a53e695503918d8
Raymond <42894676+SameriteRL@users.noreply.github.com>
2025-03-13 23:58:12 -0400

    Add dynamic router scanning
    Eliminates the need to import and include API routers one by one in the main file.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI
+import importlib
+import pkgutil
+
+from fastapi import APIRouter, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app import lifespan_func
 
-from .routers import course, scraper
+
+def scan_and_include_routers(app: FastAPI, package: str) -> None:
+    package_module = importlib.import_module(package)
+    for _, module_name, _ in pkgutil.iter_modules(package_module.__path__):
+        module = importlib.import_module(f"{package}.{module_name}")
+        for attr_name in dir(module):
+            attr = getattr(module, attr_name)
+            if isinstance(attr, APIRouter):
+                app.include_router(attr)
+
 
 app = FastAPI(root_path="/api/v1", lifespan=lifespan_func)
 app.add_middleware(
@@ -12,5 +24,5 @@ app.add_middleware(
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
-app.include_router(course.router)
-app.include_router(scraper.router)
+_package_name = __name__.split(".")[0]
+scan_and_include_routers(app, f"{_package_name}.routers")
