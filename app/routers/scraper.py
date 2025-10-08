116eaa7c4442615ee24aee4b078575a448fac099
Raymond Chen <42894676+SameriteRL@users.noreply.github.com>
2025-10-08 18:09:10 -0400

    Integrate SIS scraper with FastAPI app
    An _IS_RUNNING global variable has been introduced to the SIS scraper to prevent multiple simultaneous scraper runs.
    
    Additionally, the semaphore and connection limit per client session had to be tuned down, as connections were hitting the timeout limit when scraping all terms from 1998 to the present.
    
    The scraper should now be initiated with the dedicated /api/v1/scraper/start endpoint.

--- app/routers/scraper.py
+++ app/routers/scraper.py
@@ -8,6 +8,8 @@ router = APIRouter(prefix="/scraper")
 
 
 @router.post("/start")
-def start_scraper():
-    if not asyncio.run(sis_scraper.main()):
+async def start_scraper():
+    if sis_scraper.is_running():
         return Response(status_code=409, content="Scraper already running")
+    asyncio.create_task(sis_scraper.main())
+    return Response(status_code=202, content="Scraper started")
